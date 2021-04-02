5d1303de580460ce12d5e2a50057749dd2fe7777
Mushaik Mohamed <dongleerox12@outlook.com>
2021-04-02 16:19:48 +0530

    Home.Master Code Edite Page Load

--- Crescent POS/Home.Master.cs
+++ Crescent POS/Home.Master.cs
@@ -11,38 +11,25 @@ namespace Crescent_POS
 {
     public partial class Home : System.Web.UI.MasterPage
     {
-        public static string connectionString = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+        //public static string connectionString = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            //if (Session["user_level"].ToString() == "Admin")
-            //{
-            //    //gembakaizenrequest.Visible = true;
-            //    //userregistration.Visible = true;
-            //    //lbladmin.Visible = true;
-            //    //frmKaizenUser.Visible = false;
+            if (Session["user_level"].ToString() == "Admin")
+            {
+               
+            }
 
-            //}
+            else if (Session["user_level"].ToString() == "Management")
+            {
+               
+            }
 
-            //else if (Session["user_level"].ToString() == "Management")
-            //{
-            //    //gembakaizenrequest.Visible = false;
-            //    //userregistration.Visible = false;
-            //    //lbladmin.Visible = false;
-            //    //frmKaizenUser.Visible = false;
-            //    //frmKaizenAdmin.Visible = false;
-            //}
-
-            //else if (Session["user_level"].ToString() == "User")
-            //{
-            //    //gembakaizenrequest.Visible = false;
-            //    //userregistration.Visible = false;
-            //    //lbladmin.Visible = false;
-            //    //frmManagement.Visible = false;
-            //    //frmKaizen.Visible = false;
-            //    //frmKaizenAdmin.Visible = false;
-            //}
-            //lblfullname.Text = Session["full_name"].ToString();
+            else if (Session["user_level"].ToString() == "User")
+            {
+                
+            }
+            
         }
     }
 }
