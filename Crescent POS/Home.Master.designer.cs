3c7e4e24b66e1824816137131be31a9d63f4e248
Mushaik Mohamed <dongleerox12@outlook.com>
2021-04-02 15:46:55 +0530

    html Edite

--- Crescent POS/Home.Master.designer.cs
+++ Crescent POS/Home.Master.designer.cs
@@ -20,5 +20,14 @@ namespace Crescent_POS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblfullname;
+        
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
