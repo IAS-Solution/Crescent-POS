4e67aa190a57f69dd59b5c9473e2c552a567c459
Mushaik Mohamed <dongleerox12@outlook.com>
2021-04-06 12:07:04 +0530

    some Edites

--- Crescent POS/Users.aspx.designer.cs
+++ Crescent POS/Users.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace Crescent_POS {
         protected global::System.Web.UI.WebControls.Button btnSave;
         
         /// <summary>
+        /// btnupdate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnupdate;
+        
+        /// <summary>
         /// txtSearch control.
         /// </summary>
         /// <remarks>
